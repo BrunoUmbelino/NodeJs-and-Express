e8ae2448b77b8cde919c4a4f17210acf1fb36e91
Bruno <bruno.umbelino0@gmail.com>
2021-05-10 20:06:37 -0300

    adding fs-sync topic2

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -4,7 +4,7 @@ const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
 writeFileSync(
-  "./content/result-sync.txt ",
+  "./content/result.txt",
   `Here is the result ${first} , ${second}`,
   { flag: 'a'}
 );
