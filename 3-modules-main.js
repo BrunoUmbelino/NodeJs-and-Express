49698bfe3886159898b36547dca72c44e6cd393b
Bruno <bruno.umbelino0@gmail.com>
2021-05-10 18:35:22 -0300

    alternative export modules

--- 3-modules-main.js
+++ 3-modules-main.js
@@ -1,7 +1,9 @@
 const names = require('./3-modules-variables')
 const sayHi = require('./3-modules-function')
+const data = require('./6-modules-alternative')
 
 sayHi('maria')
 sayHi(names.bruno)
 sayHi(names.umbelino)
 
+console.log(data)
